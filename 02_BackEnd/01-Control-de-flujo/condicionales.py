c6964c301604444a800697bc3f5c77fa36ea5ccc
oiab94NotebookHP <oscar.alonso.994@gmail.com>
2022-07-05 21:10:11 -0400

    (BackEnd) PYTHON: Implementación de la sentencia IF

---
+++ 02_BackEnd/01-Control-de-flujo/condicionales.py
@@ -0,0 +1,12 @@
+# Los bloques en las sentencias condicionales se definen por la identación, es 
+# decir tenemos que dejar un sangrado parar indicar el código dentro del la 
+# condicional
+
+#--------------#
+# Sentencia IF #
+#--------------#
+a = 5
+b = 4
+if (a > b):
+	print('El código se ejecuta dentro del IF')
+print('Este código se ejecutara siempre')
