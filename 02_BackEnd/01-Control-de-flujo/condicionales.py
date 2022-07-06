6d1b3211497a9fa565eb49b10711c333b3c77fc9
oiab94NotebookHP <oscar.alonso.994@gmail.com>
2022-07-05 21:15:53 -0400

    (BackEnd) PYTHON: Implementación sentencia IF..ELSE

--- 02_BackEnd/01-Control-de-flujo/condicionales.py
+++ 02_BackEnd/01-Control-de-flujo/condicionales.py
@@ -7,6 +7,16 @@
 #--------------#
 a = 5
 b = 4
+print('\nSentencia IF')
 if (a > b):
 	print('El código se ejecuta dentro del IF')
 print('Este código se ejecutara siempre')
+
+#---------------------#
+# Sentencia IF...ELSE #
+#---------------------#
+print('\nSentencia IF...ELSE')
+if (a > b):
+	print('A > B')
+else:
+	print('B > A')
